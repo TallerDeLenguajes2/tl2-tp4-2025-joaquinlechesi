c62dcf80210a832951607fe6049ecf54665a9c8a
joaquinlechesi <joaquinlechesi@gmail.com>
2025-10-04 00:20:51 -0300

    Se creo la clase AccesoADatosPedidos y se le agrego el metodo (que esta incompleto) List<Pedidos> Obtener() y Guardar(List<Pedidos> Pedidos)

---
+++ MiWebAPI/Models/AccesoADatosPedidos.cs
@@ -0,0 +1,15 @@
+namespace MiCadeteria
+{
+    public class AccesoADatosPedidos
+    {
+        public List<Pedidos> Obtener()
+        {
+
+            return new List<Pedidos>(); //dejo de esta forma para que no marque error
+        }
+        public void Guardar(List<Pedidos> Pedidos)
+        {
+            //incompleto
+        }
+    }
+}
