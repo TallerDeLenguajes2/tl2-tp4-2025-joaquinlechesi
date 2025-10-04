ce322eae0f1d2aab20ae83e60678c645df2032ec
joaquinlechesi <joaquinlechesi@gmail.com>
2025-10-04 00:15:13 -0300

    Se creo la clase AccesoADatosCadete y se le agrego el metodo (que esta incompleto) List<Cadete> Obtener()

---
+++ MiWebAPI/Models/AccesoADatosCadete.cs
@@ -0,0 +1,11 @@
+namespace MiCadeteria
+{
+    public class AccesoADatosCadete
+    {
+        public List<Cadete> Obtener()
+        {
+
+            return new List<Cadete>(); //dejo de esta forma para que no marque error
+        }
+    }
+}
